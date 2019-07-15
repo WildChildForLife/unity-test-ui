09e4034787d2ab50e52a3f284a0a04b74fce9f17
Youssef El Gharbaoui <youssef.el-gharbaoui-prestataire@laposte.fr>
2019-07-15 17:11:25 +0200

    All Create views ready + Logs in place + Routes ready

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
+// Well you know, express
 const express = require('express');
+// Http Body Parser
+const bodyParser = require('body-parser');
+// Fetch (for the API)
 const fetch = require('node-fetch');
+// Dates Handle
 const moment = require('moment');
+// Logs
+const winston = require('winston');
+// Cookie Parser
+const cookieParser = require('cookie-parser');
+// Sessions
+const session = require('express-session');
+// Flash messages
+const flash = require('express-flash');
+
+
+
 const app = express();
+const sessionStore = new session.MemoryStore;
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cookieParser('secret'));
+app.use(session({
+    cookie: { maxAge: 60000 },
+    store: sessionStore,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'secret'
+}));
+app.use(flash());
+
+// to support JSON-encoded bodies
+// to support URL-encoded bodies
 // API params
 const apiProtocole = 'http';
 const apiBaseUrl = '127.0.0.1';
@@ -14,10 +46,36 @@ const apiUrl = apiProtocole + '://' + apiBaseUrl + ':' + apiPort;
 const apiUnreachableException = 'API Unreachable. Please make sure that the API server is up and running on port 3000.';
 const datesFormat = 'YYYY-MM-DD';
 
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' })
+    ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+}
+
 const server = app.listen(7000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
 
+app.use((req, res, next) => {
+    // if there's a flash message in the session request, make it available in the response, then delete it
+    res.locals.sessionFlash = req.session.sessionFlash;
+    delete req.session.sessionFlash;
+    next();
+});
+
+// -------------------------------- PLAYERS --------------------------------
+// GET : PLAYERS
 app.get('/players', (req, res) => {
     fetch(apiUrl + '/players').then((apiResponse) => apiResponse.json()).then((apiResponse) => {
         res.render('players', {
@@ -30,6 +88,38 @@ app.get('/players', (req, res) => {
     });
 });
 
+// POST : PLAYER
+app.post('/players/create', (httpRequest, httpResponse) => {
+    let bodyRequest = parseRequest(httpRequest.body);
+
+    fetch(apiUrl + '/players', {
+        method: 'POST',
+        body: JSON.stringify(bodyRequest),
+        headers: { 'Content-Type': 'application/json'}
+    }).then(responseApi => responseApi.json()).then(responseApi => {
+        httpRequest.flash('apiResponse', responseApi);
+        httpResponse.redirect('/players/create');
+    }).catch((e) => {
+        throw new Error(apiUnreachableException);
+    });
+
+});
+
+// GET : PLAYER
+app.get('/players/create', (req, res) => {
+    fetch(apiUrl + '/sessions').then((apiResponse) => apiResponse.json()).then((apiResponse) => {
+        res.render('player-create', {
+            title: 'Player - Create : ',
+            activeItem: 'players',
+            sessions: apiResponse,
+            apiResponse: req.flash('apiResponse'),
+        });
+    }).catch((e) => {
+        throw new Error(apiUnreachableException);
+    });
+});
+
+// GET : PLAYER BY ID
 app.get('/players/:id', (req, res) => {
     fetch(apiUrl + '/players' + '/' + req.params.id).then((apiResponse) => apiResponse.json()).then((apiResponse) => {
         res.render('player', {
@@ -42,6 +132,8 @@ app.get('/players/:id', (req, res) => {
     });
 });
 
+// -------------------------------- SESSIONS --------------------------------
+// GET : SESSIONS
 app.get('/sessions', (req, res) => {
     fetch(apiUrl + '/sessions').then((apiResponse) => apiResponse.json()).then((apiResponse) => {
         // Transform Dates to readable dates
@@ -60,6 +152,34 @@ app.get('/sessions', (req, res) => {
     });
 });
 
+// POST : SESSION
+app.post('/sessions/create', (httpRequest, httpResponse) => {
+    let bodyRequest = parseRequest(httpRequest.body);
+
+    fetch(apiUrl + '/sessions', {
+        method: 'POST',
+        body: JSON.stringify(bodyRequest),
+        headers: { 'Content-Type': 'application/json'}
+    }).then(responseApi => responseApi.json()).then(responseApi => {
+        httpRequest.flash('apiResponse', responseApi);
+        httpResponse.redirect('/sessions/create');
+    }).catch((e) => {
+        throw new Error(apiUnreachableException);
+    });
+
+});
+
+// GET : SESSION
+app.get('/sessions/create', (req, res) => {
+    res.render('session-create', {
+        title: 'Session - Create : ',
+        activeItem: 'sessions',
+        apiResponse: req.flash('apiResponse'),
+    });
+});
+
+
+// GET : SESSION BY ID
 app.get('/sessions/:id', (req, res) => {
     fetch(apiUrl + '/sessions' + '/' + req.params.id).then((apiResponseSession) => apiResponseSession.json()).then((apiResponseSession) => {
         // Convert Session Date
@@ -87,19 +207,15 @@ app.get('/sessions/:id', (req, res) => {
 
 });
 
+// -------------------------------- RATINGS --------------------------------
+// GET : RATINGS
 app.get('/ratings', (req, res) => {
-    //const person = people.profiles.find(p => p.id === req.query.id);
     fetch(apiUrl + '/ratings').then((apiResponse) => apiResponse.json()).then((apiResponse) => {
-        console.log(apiResponse);
         if (req.query.sessionId !== undefined) {
             const filtered = apiResponse.find(r => r.sessionId == req.query.sessionId);
-            if (filtered !== undefined) {
-
-            }
             apiResponse = (filtered !== undefined) ? [filtered] : apiResponse;
-        }
 
-        console.log(apiResponse);
+        }
 
         res.render('ratings', {
             title: 'List Ratings :',
@@ -111,6 +227,44 @@ app.get('/ratings', (req, res) => {
     });
 });
 
+// POST : CREATE RATING
+app.post('/ratings/create', (httpRequest, httpResponse) => {
+    let bodyRequest = parseRequest(httpRequest.body);
+
+    console.log(bodyRequest);
+    fetch(apiUrl + '/ratings', {
+        method: 'POST',
+        body: JSON.stringify(bodyRequest),
+        headers: { 'Content-Type': 'application/json'}
+    }).then(responseApi => responseApi.json()).then(responseApi => {
+        httpRequest.flash('apiResponse', responseApi);
+        httpResponse.redirect('/ratings/create');
+    }).catch((e) => {
+        throw new Error(apiUnreachableException);
+    });
+
+});
+
+// GET : CREATE RATING
+app.get('/ratings/create', (req, res) => {
+    fetch(apiUrl + '/sessions').then((apiSessionResponse) => apiSessionResponse.json()).then((apiSessionResponse) => {
+        fetch(apiUrl + '/players').then((apiPlayersResponse) => apiPlayersResponse.json()).then((apiPlayersResponse) => {
+            res.render('rating-create', {
+                title: 'Rating - Create : ',
+                activeItem: 'ratings',
+                sessions: apiSessionResponse,
+                players: apiPlayersResponse,
+                apiResponse: req.flash('apiResponse'),
+            });
+        }).catch((e) => {
+            throw new Error(apiUnreachableException);
+        });
+    }).catch((e) => {
+        throw new Error(apiUnreachableException);
+    });
+});
+
+// GET : RATING BY ID
 app.get('/ratings/:id', (req, res) => {
     fetch(apiUrl + '/ratings' + '/' + req.params.id).then((apiResponse) => apiResponse.json()).then((apiResponse) => {
         res.render('ratings', {
@@ -124,6 +278,8 @@ app.get('/ratings/:id', (req, res) => {
 
 });
 
+// -------------------------------- COMMENTS --------------------------------
+// GET : COMMENTS
 app.get('/comments', (req, res) => {
     fetch(apiUrl + '/comments').then((apiResponse) => apiResponse.json()).then((apiResponse) => {
         // Transform Dates to readable dates
@@ -142,6 +298,43 @@ app.get('/comments', (req, res) => {
     });
 });
 
+// POST : CREATE COMMENT
+app.post('/comments/create', (httpRequest, httpResponse) => {
+    let bodyRequest = parseRequest(httpRequest.body);
+
+    fetch(apiUrl + '/comments', {
+        method: 'POST',
+        body: JSON.stringify(bodyRequest),
+        headers: { 'Content-Type': 'application/json'}
+    }).then(responseApi => responseApi.json()).then(responseApi => {
+        httpRequest.flash('apiResponse', responseApi);
+        httpResponse.redirect('/comments/create');
+    }).catch((e) => {
+        throw new Error(apiUnreachableException);
+    });
+
+});
+
+// GET : CREATE COMMENT
+app.get('/comments/create', (req, res) => {
+    fetch(apiUrl + '/sessions').then((apiSessionResponse) => apiSessionResponse.json()).then((apiSessionResponse) => {
+        fetch(apiUrl + '/players').then((apiPlayersResponse) => apiPlayersResponse.json()).then((apiPlayersResponse) => {
+            res.render('comment-create', {
+                title: 'Comment - Create : ',
+                activeItem: 'comments',
+                sessions: apiSessionResponse,
+                players: apiPlayersResponse,
+                apiResponse: req.flash('apiResponse'),
+            });
+        }).catch((e) => {
+            throw new Error(apiUnreachableException);
+        });
+    }).catch((e) => {
+        throw new Error(apiUnreachableException);
+    });
+});
+
+// GET : COMMENT BY ID
 app.get('/comments/:id', (req, res) => {
     fetch(apiUrl + '/comments' + '/' + req.params.id).then((apiResponse) => apiResponse.json()).then((apiResponse) => {
         res.render('comment', {
@@ -152,5 +345,20 @@ app.get('/comments/:id', (req, res) => {
     }).catch((e) => {
         throw new Error(apiUnreachableException);
     });
-
 });
+
+const parseRequest = (bodyRequest) => {
+    if (bodyRequest !== null && bodyRequest.constructor.name === "Object" && Object.keys(bodyRequest).length) {
+        Object.keys(bodyRequest).forEach((element) => {
+            if (element.slice(-2) === 'Id' || element === 'score') {
+                bodyRequest[element] = parseInt(bodyRequest[element]);
+            }
+            if (element === 'createdAt') {
+                bodyRequest[element] = moment().toISOString();
+            }
+        });
+    }
+
+    return bodyRequest;
+}
+
